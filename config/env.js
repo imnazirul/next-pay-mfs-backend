a7f5707009552d01aab882a66d4663b28654f240
Nazirul <nazirul.me01c@gmail.com>
2025-02-27 05:10:11 +0600

    Comment out dotenv configuration in env.js to prevent loading environment variables

--- config/env.js
+++ config/env.js
@@ -1,5 +1,5 @@
-import {config} from "dotenv"
+// import {config} from "dotenv"
 
-config({path: `.env.${process.env.NODE_ENV || "production"}.local`})
+// config({path: `.env.${process.env.NODE_ENV || "production"}.local`})
 
 export const {NODE_ENV, DB_URI, PORT, JWT_SECRET, JWT_EXPIRES_IN} =  process.env
